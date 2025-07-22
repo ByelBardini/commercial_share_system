a67d8327c7941a683bae4b7521897d5fb261b46c
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-07-22 16:55:17 -0300

    feat: adiciona função de deletar e modificar cidade, além de verificação de token

--- server/controllers/cidadeController.js
+++ server/controllers/cidadeController.js
@@ -27,3 +27,34 @@ export const postCidade = (req, res) => {
         res.status(201).json({ message: "Cidade cadastrada com sucesso!" });
     });
 }
+
+export const putCidade = (req, res) =>{
+    const { id } = req.params;
+    const { cidade_nome, cidade_uf } = req.body;
+
+    if (!cidade_nome || !cidade_uf) {
+        return res.status(400).json({ error: "Nome e UF da cidade são obrigatórios." });
+    }
+
+    const sql = `UPDATE cidades SET cidade_nome = ?, cidade_uf = ? WHERE cidade_id = ?`;
+    db.query(sql, [cidade_nome, cidade_uf, id], (err, results) => {
+        if (err) {
+            console.error("Erro ao atualizar cidade:", err);
+            return res.status(500).json({ error: "Erro ao atualizar cidade" });
+        }
+        res.json({ message: "Cidade atualizada com sucesso!" });
+    });
+}
+
+export const deleteCidade = (req, res) => {
+    const { id } = req.params;
+
+    const sql = `DELETE FROM cidades WHERE cidade_id = ?`;
+    db.query(sql, [id], (err, results) => {
+        if (err) {
+            console.error("Erro ao deletar cidade:", err);
+            return res.status(500).json({ error: "Erro ao deletar cidade" });
+        }
+        res.json({ message: "Cidade deletada com sucesso!" });
+    });
+}
--- server/routes/cidadeRoutes.js
+++ server/routes/cidadeRoutes.js
@@ -1,9 +1,12 @@
-import { getCidades, postCidade } from "../controllers/authController.js";
+import { getCidades, postCidade, putCidade, deleteCidade } from "../controllers/authController.js";
+import verificaToken from "../middlewares/verificaToken.js";
 import express from "express";
 
 const router = express.Router();
 
-router.get("/cidade", getCidades);
-router.post("/cidade", postCidade);
+router.get("/cidade", verificaToken, getCidades);
+router.post("/cidade", verificaToken, postCidade);
+router.put("/cidade/:id", verificaToken, putCidade);
+router.delete("/cidade/:id", verificaToken, deleteCidade);
 
 export default router;
