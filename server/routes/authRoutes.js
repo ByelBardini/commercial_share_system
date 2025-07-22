c44cd80edb9b7ca2c6a5ebda13089e0aa1a909a2
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-07-22 16:54:56 -0300

    fix: retira verificação que foi no arquivo errado

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,9 @@
 import { login, logout } from "./../controllers/authController.js";
-import verificaToken from "../middlewares/verificaToken.js";
 import express from "express";
 
 const router = express.Router();
 
-router.post("/login", verificaToken, login);
-router.get("/logout", verificaToken, logout);
+router.post("/login", login);
+router.get("/logout", logout);
 
 export default router;
