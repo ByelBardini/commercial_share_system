bd4c34e45221a27ce169b25f2f2038436865448d
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-07-22 18:04:48 -0300

    feat: adiciona tela de login

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,39 @@
+function Login() {
+  return (
+    <div className="bg-blue-700 min-h-screen w-screen flex justify-center items-center p-6">
+      <div className="bg-white w-1/3 max-h-[90vh] rounded-lg shadow-lg gap-2 overflow-auto p-2">
+        <img
+          src="./src/assets/empresa-logo.png"
+          alt="Logo"
+          className=" w-1/2 mx-auto mt-4 rounded-lg shadow-lg bg-transparent p-2 transition duration-300 ease-in-out hover:scale-105"
+        />
+        <div className="flex flex-col items-center justify-center h-full p-2">
+          <h1 className="text-2xl font-bold mb-4">Login</h1>
+          <input
+            type="text"
+            placeholder="Usuário"
+            id="usuario_login"
+            className=" border-gray-500 border-2 w-11/12 rounded-md text-gray-800 px-4 py-2 text-2xl active:bg-gray-100 focus:outline-none focus:ring-0 mb-6"
+          />
+          <h1 className="text-2xl font-bold mb-4">Senha</h1>
+          <input
+            type="password"
+            placeholder="Senha"
+            id="usuario-senha"
+            className=" border-gray-500 border-2 w-11/12 rounded-md text-gray-800 px-4 py-2 text-2xl active:bg-gray-100 focus:outline-none focus:ring-0 mb-6"
+          />
+          <button
+            className="bg-blue-700 text-white font-bold px-6 py-2 rounded-md text-xl hover:bg-blue-900 transition duration-300 ease-in-out"
+            onClick={() => alert("Botão Clicado")}
+          >
+            Login
+          </button>
+          <h1 className="text-sm font-bold mb-4 text-center px-2">
+            Para conseguir seu login e senha, entre em contato com o setor de TI
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Login;
