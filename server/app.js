68f92d22b910d2d84a9b26d587730423aee878b4
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-07-22 16:34:08 -0300

    feat: adiciona função de login e verificação de token jwt

--- server/app.js
+++ server/app.js
@@ -1,12 +1,32 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config({path: '../.env'});
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1 * 1000 * 60 * 24,
+      httpOnly: true,
+      sameSite: "Lax",
+      secure: false,
+      path: "/",
+    },
+  })
+);
 app.use(cors());
+
+app.use(authRoutes);
 
 export default app;
---
+++ server/controllers/authController.js
@@ -0,0 +1,63 @@
+import jwt from "jsonwebtoken";
+import db from "../config/db.js";
+import bcrypt from "bcrypt";
+
+const CHAVE = process.env.SECRET_KEY_LOGIN;
+
+export const login = (req, res) => {
+  const { login_usuario, senha_usuario } = req.body;
+
+  if (!login_usuario || !senha_usuario) {
+    return res.status(400).json({ error: "Login e senha são obrigatórios." });
+  }
+
+  const sql = `SELECT * FROM usuarios WHERE usuario_login = ?`;
+  db.query(sql, [login_usuario], (err, results) => {
+    if (err) {
+      console.error("Erro na consulta:", err);
+      return res.status(500).json({ error: "Erro ao validar usuário" });
+    }
+
+    if (results.length > 0) {
+      bcrypt.compare(senha_usuario, results[0].usuario_senha, (err, match) => {
+        if (err) {
+          console.error("Erro ao comparar senhas:", err);
+          return res.status(500).json({ error: "Erro ao validar senha" });
+        }
+
+        if (match) {
+          const payload = {
+            usuario_nome: results[0].usuario_nome
+          };
+
+          const token = jwt.sign(payload, CHAVE, { expiresIn: "2h" });
+          const userSession = { usuario_id: results[0].usuario_id };
+
+          req.session.user = userSession
+          res.cookie("token", token, {
+            httpOnly: true,
+            sameSite: "Lax",
+            secure: false,
+            path: "/",
+            maxAge: 2 * 60 * 60 * 1000,
+          });
+        } else {
+          res.status(401).json({ error: "Usuário ou senha inválidos." });
+        }
+      });
+    } else {
+      res.status(401).json({ error: "Usuário ou senha inválidos." });
+    }
+  });
+};
+
+export const logout = (req, res) => {
+    req.session.destroy((err) => {
+    if (err) {
+      console.log("Erro ao destruir sessão:", err);
+      return res.status(500).json({ error: "Erro ao realizar logout" });
+    }
+  });
+  res.clearCookie("token");
+  res.json({ mensagem: "Logout realizado com sucesso" });
+};
