fcee43a93aeb94803df0ecceb8be97f962b090b5
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-07-22 16:52:03 -0300

    refactot: adicionado verificação durante ações de listagem e adicionar cidades

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import { login, logout } from "./../controllers/authController.js";
+import verificaToken from "../middlewares/verificaToken.js";
 import express from "express";
 
 const router = express.Router();
 
-router.post("/login", login);
-router.get("/logout", logout);
+router.post("/login", verificaToken, login);
+router.get("/logout", verificaToken, logout);
 
 export default router;
