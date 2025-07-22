10cb8df36d537510598a4b7ca777479402c579c7
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-07-22 13:51:24 -0300

    refactor: início do backend

---
+++ server/app.js
@@ -0,0 +1,11 @@
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config({path: '../.env'});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+export default app;
