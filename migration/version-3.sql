494e1bea661d6cb5884b141b327547cbc7e74b03
Gabryel Bardini <128429834+ByelBardini@users.noreply.github.com>
2025-07-23 14:31:52 -0300

    refactor: adicionado campo de observação e datas nas associações, adicionado tabela de contatos

---
+++ migration/version-3.sql
@@ -0,0 +1,18 @@
+ALTER TABLE `share_comercial`.`associacoes` 
+ADD COLUMN `associacao_data_contato` DATE NULL AFTER `associacao_cnpj`,
+ADD COLUMN `associcacao_data_fechamento` DATE NULL AFTER `associacao_data_contato`,
+ADD COLUMN `associacao_observacao` TEXT NULL AFTER `associcacao_data_fechamento`,
+CHANGE COLUMN `associacao_nome_fantasia` `associacao_nome_fantasia` VARCHAR(100) NULL ;
+
+CREATE TABLE `share_comercial`.`contatos` (
+  `contato_id` INT NOT NULL AUTO_INCREMENT,
+  `contato_associacao_id` INT NOT NULL,
+  `contato_tipo` ENUM('telefone', 'email') NOT NULL,
+  `contato` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`contato_id`),
+  INDEX `contato_associacao_id_idx` (`contato_associacao_id` ASC) VISIBLE,
+  CONSTRAINT `contato_associacao_id`
+    FOREIGN KEY (`contato_associacao_id`)
+    REFERENCES `share_comercial`.`associacoes` (`associacao_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
